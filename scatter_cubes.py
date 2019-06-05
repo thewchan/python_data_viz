b0dc101cbf4725ed08e31f81c463c6482e9f5442
thewchan <thewchan@gmail.com>
2019-06-05 10:50:16 -0700

    15-1 Cubes: Part 2

--- scatter_cubes.py
+++ scatter_cubes.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 
 
-x_values = range(1, 5)
+x_values = range(1, 5000)
 y_values = [x**3 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=50)
+ax.scatter(x_values, y_values, s=10)
 
 ax.set_title("Cube Numbers", fontsize=24)
 ax.set_xlabel("Values", fontsize=14)
 ax.set_ylabel("Cubed Values", fontsize=14)
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-plt.savefig("five_cube.png", bbox_inches='tight')
+plt.savefig("five_thousand_cube.png", bbox_inches='tight')
 plt.show()
