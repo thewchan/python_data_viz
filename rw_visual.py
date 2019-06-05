c816705abf14aa424927ca9b69fe1df935738002
thewchan <thewchan@gmail.com>
2019-06-05 11:16:15 -0700

    Continuous Random Walk

--- rw_visual.py
+++ rw_visual.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 
-# Make a random walk.
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
 
-plt.savefig('rw_plot.png', bbox_inches='tight')
-plt.show()
+    plt.savefig('rw_plot.png', bbox_inches='tight')
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
