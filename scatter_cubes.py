14c67cc6aa63fc31cd97fb2be38eefe3cbfc3980
thewchan <thewchan@gmail.com>
2019-06-05 10:56:34 -0700

    15-2 Colored Cubes

--- scatter_cubes.py
+++ scatter_cubes.py
@@ -6,12 +6,12 @@ y_values = [x**3 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
 
 ax.set_title("Cube Numbers", fontsize=24)
 ax.set_xlabel("Values", fontsize=14)
 ax.set_ylabel("Cubed Values", fontsize=14)
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-plt.savefig("five_thousand_cube.png", bbox_inches='tight')
+plt.savefig("five_thousand_cube_cmap.png", bbox_inches='tight')
 plt.show()
