c680e4cfa9b147055b597a08980ec181b3c423d4
thewchan <thewchan@gmail.com>
2019-06-06 08:54:40 -0700

    15-4 Modified Random Walks

--- random_walk.py
+++ random_walk.py
@@ -20,11 +20,11 @@ class RandomWalk:
 
             # Decide which direction to go and how far to go in that direction.
             x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
+            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
             x_step = x_direction * x_distance
 
             y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
+            y_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
             y_step = y_direction * y_distance
 
             # Reject moves that go nowhere.
