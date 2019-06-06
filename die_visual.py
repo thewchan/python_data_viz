c1a36bd446b76c9b990a59de7ff887a1967a587c
thewchan <thewchan@gmail.com>
2019-06-06 10:15:23 -0700

    15-9 Adding List Comprehension to Dice

--- die_visual.py
+++ die_visual.py
@@ -10,17 +10,24 @@ die_2 = Die()
 die_3 = Die()
 
 # Make some rolls, and store results in a list.
-results = []
-for roll_num in range(50_000):
-    result = die_1.roll() + die_2.roll() + die_3.roll()
-    results.append(result)
+# results = []
+# for roll_num in range(50_000):
+#     result = die_1.roll() + die_2.roll() + die_3.roll()
+#     results.append(result)
+
+results = (
+    [(die_1.roll()+die_2.roll()+die_3.roll()) for roll_num in range(50_000)]
+    )
 
 # Analyze the results.
-frequencies = []
+
 max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
-for value in range(3, max_result+1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+# frequencies = []
+# for value in range(3, max_result+1):
+#     frequency = results.count(value)
+#     frequencies.append(frequency)
+
+frequencies = [results.count(value) for value in range(3, max_result+1)]
 
 # Visualize the results.
 x_values = list(range(3, max_result+1))
