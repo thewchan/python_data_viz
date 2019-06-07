1cf701285fac8601a7e7ed75c04db61e8d2b666e
thewchan <thewchan@gmail.com>
2019-06-07 12:55:03 -0700

    Extracting, reading, and plotting data

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 
 
 filename = 'data/sitka_weather_07-2018_simple.csv'
@@ -6,5 +7,24 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    # Get high temperatures from this file.
+    highs = []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+
+print(highs)
+
+# Plot the high temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot.
+plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.savefig('sitka_highs.png', bbox_inches='tight')
+plt.show()
