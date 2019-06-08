3f98eaaf75d30053877cf3e5d3aba27694e56706
thewchan <thewchan@gmail.com>
2019-06-08 08:46:21 -0700

    Checking Repository Dictionary Keys

--- python_repos.py
+++ python_repos.py
@@ -9,6 +9,14 @@ print(f"Status code: {r.status_code}")
 
 # Store API response in a variable.
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
 
-# Process result.
-print(response_dict.keys())
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned {len(repo_dicts)}")
+
+# Examine the first repository.
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
