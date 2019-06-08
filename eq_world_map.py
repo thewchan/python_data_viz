94e1c8db639a28fa0b8403ca84fdcb242e9136f9
thewchan <thewchan@gmail.com>
2019-06-07 17:22:24 -0700

    Customizing Marker Colors

--- eq_world_map.py
+++ eq_world_map.py
@@ -5,7 +5,8 @@ from plotly import offline
 
 
 # Explore the structure of the data.
-filename = 'data/eq_data_1_day_m1.json'
+# filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -27,9 +28,13 @@ data = ([{
     'lat': lats,
     'marker': ({
         'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
         }),
     }])
 my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data': data, 'layout': my_layout}
-offline.plot(fig, filename='global_earthquakes.html')
+offline.plot(fig, filename='global_earthquakes_30days.html')
